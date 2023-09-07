8ef698d011b1e7c741ea0b2d37d249b0e8cefd57
Brandon Hundt <brandon.hundt@prx.org>
2023-09-07 13:52:57 -0500

    feat(scroll-bg): convert initial jquery to vanilla js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,26 @@
-function position(el) {
-  const {top, left} = el.getBoundingClientRect();
-  const {marginTop, marginLeft} = getComputedStyle(el);
-  return {
-    top: top - parseInt(marginTop, 10),
-    left: left - parseInt(marginLeft, 10)
-  };
-}
+window.addEventListener("scroll", function() {
+  // selectors
+  var body = document.body,
+      panels = document.querySelectorAll('.scroll-bg');
 
-function scrollTop(el, value) {
-  var win;
-  if (el.window === el) {
-    win = el;
-  } else if (el.nodeType === 9) {
-    win = el.defaultView;
-  }
+  // Iterate through each panel
+  panels.forEach(function (panel) {
+    var rect = panel.getBoundingClientRect();
 
-  if (value === undefined) {
-    return win ? win.pageYOffset : el.scrollTop;
-  }
+    // Calculate the top and bottom scroll positions for this panel
+    var panelTop = rect.top + window.pageYOffset;
+    var panelBottom = panelTop + rect.height;
 
-  if (win) {
-    win.scrollTo(win.pageXOffset, value);
-  } else {
-    el.scrollTop = value;
-  }
-}
+    // Check if the current scroll position is within the panel's range
+    if (window.pageYOffset >= panelTop && window.pageYOffset < panelBottom) {
+      // Remove all classes on body with color-
+      body.className = body.className.replace(/\bcolor-\S+/g, '');
 
-function updateBackgroundColor() {
-  const body = this.document.querySelector('body');
-  const panels = this.document.getElementsByClassName('scroll-bg');
-
-  const scroll = scrollTop(window) + (window.innerHeight / 3);
-
-  for (const panel of panels) {
-    if (position(panel).top <= scroll && position(panel).top + panel.offsetHeight > scroll) {
-      body.classList.forEach(clss => {
-        if (clss.startsWith('color-')) {
-          body.classList.remove(clss);
-        }
-      })
-
-      body.classList.add(`color-${panel.getAttribute('data-color')}`)
+      // Add class of the currently active div
+      body.classList.add('color-' + panel.getAttribute('data-color'));
     }
-  }
-}
+  });
+});
 
-window.addEventListener('scroll', updateBackgroundColor);
-
-(function () {
-  document.addEventListener('DOMContentLoaded', async (_) => {
-    updateBackgroundColor();
-  })
-})();
+// Trigger the scroll event once to apply the initial state
+window.dispatchEvent(new Event('scroll'));
