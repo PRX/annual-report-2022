7e8334ffc4f74905225dc1347ccea9cd1bcaff0e
Brandon Hundt <brandon.hundt@prx.org>
2023-09-11 12:33:48 -0500

    feat(scroll-bg): change to use more perfromant intersection

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,23 @@
-window.addEventListener("scroll", function() {
-  // selectors
-  var body = document.body,
-      panels = document.querySelectorAll('.scroll-bg');
-
-  // Calculate the offset to start changing colors (33%)
-  var offset = window.innerHeight / 3;
-
-  // Iterate through each panel
-  panels.forEach(function (panel) {
-    var rect = panel.getBoundingClientRect();
-
-    // Calculate the top and bottom scroll positions for this panel
-    var panelTop = rect.top + window.pageYOffset - offset; // Adjusted with offset
-    var panelBottom = panelTop + rect.height;
-
-    // Check if the current scroll position is within the panel's range
-    if (window.pageYOffset >= panelTop && window.pageYOffset < panelBottom) {
-      // Remove all classes on body with color-
-      body.className = body.className.replace(/\bcolor-\S+/g, '');
-
-      // Add class of the currently active div
-      body.classList.add('color-' + panel.getAttribute('data-color'));
+// Function to handle changes in intersection
+function handleIntersection(entries, observer) {
+  entries.forEach(function (entry) {
+   console.log(entry.isIntersecting);
+    if (entry.isIntersecting) {
+      console.log(entry.target.getAttribute('data-color'));
+      document.body.setAttribute('data-theme', entry.target.getAttribute('data-color'));
     }
   });
+}
+
+// Create an Intersection Observer
+var observer = new IntersectionObserver(handleIntersection, {
+  //threshold: 0.33, // Trigger when 33% of the target is visible
 });
 
-// Trigger the scroll event once to apply the initial state
-window.dispatchEvent(new Event('scroll'));
+// Select all panels with the class 'scroll-bg'
+var panels = document.querySelectorAll('[data-color]');
+
+// Observe each panel
+panels.forEach(function (panel) {
+  observer.observe(panel);
+});
