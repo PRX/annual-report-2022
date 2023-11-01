094c0177dbcdac1af25482b809fe6c6c4b5d9c13
Brandon Hundt <brandonhundt@gmail.com>
2023-11-01 13:51:42 -0500

    feat(finacials): adjust color and chart style

--- assets/js/highcharts.js
+++ assets/js/highcharts.js
@@ -26,7 +26,7 @@
 			type: 'pie'
 		},
 		title: {
-			align: 'left'
+			align: 'center'
 		},
 		tooltip: {
 			pointFormat: '{series.name}: <b>${point.y:,.2f}</b>'
@@ -47,17 +47,13 @@
 				cursor: 'pointer',
 				colors,
 				borderRadius: 5,
+				showInLegend: true,
 				dataLabels: {
-					enabled: true,
-					format: '<b>{point.name}</b><br>${point.y:,.2f}',
-					distance: -50,
-					filter: {
-						property: 'percentage',
-						operator: '>',
-						value: 4
-					}
+					enabled: false,
 				}
-			}
+				
+			},
+			
 		}
 	};
 
@@ -71,11 +67,11 @@
 		series: [{
 			name: 'Revenue',
 			data: [
-				{ name: 'Underwriting', y: 12801442 },
-				{ name: 'Operating Activities', y: 11567053 },
-				{ name: 'Grants', y: 4729519 },
-				{ name: 'Individual Grants', y: 1641345 },
-				{ name: 'Other', y: 1325191 }
+				{ name: 'Underwriting', y: 12801442, color: '#003A57' },
+				{ name: 'Operating Activities', y: 11567053, color: '#CA3D15' },
+				{ name: 'Grants', y: 4729519, color: '#0089BD' },
+				{ name: 'Individual Grants', y: 1641345, color: '#FB9306' },
+				{ name: 'Other', y: 1325191, color: '#A1C5E2' }
 			]
 		}]
 	});
@@ -89,10 +85,10 @@
 		series: [{
 			name: 'Expenses',
 			data: [
-				{ name: 'Support for Creators', y: 15061154 },
-				{ name: 'Content Production and Distribution', y: 19388411 },
-				{ name: 'Administrative', y: 2845813 },
-				{ name: 'Fundraising', y: 1519341 }
+				{ name: 'Support for Creators', y: 15061154, color: '#003A57' },
+				{ name: 'Content Production and Distribution', y: 19388411, color: '#CA3D15'},
+				{ name: 'Administrative', y: 2845813, color: '#0089BD' },
+				{ name: 'Fundraising', y: 1519341, color: '#A1C5E2' }
 			]
 		}]
 	});
